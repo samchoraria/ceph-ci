8ddd25e33f884598c56ec60395b14db38bf51364
Abhishek Lekshmanan <abhishek@suse.com>
2017-08-25 10:48:54 +0200

    rgw_tag: implement emplace & clear methods
    Useful when populating tags from XML
    
    Signed-off-by: Abhishek Lekshmanan <abhishek@suse.com>

--- src/rgw/rgw_tag.h
+++ src/rgw/rgw_tag.h
@@ -30,9 +30,11 @@ class RGWObjTags
 
   void dump(Formatter *f) const;
   bool add_tag(const std::string& key, const std::string& val="");
+  bool emplace_tag(std::string&& key, std::string&& val);
   int check_and_add_tag(const std::string& key, const std::string& val="");
   size_t count() const {return tag_map.size();}
   int set_from_string(const std::string& input);
+  void clear() { tag_map.clear(); }
   const tag_map_t& get_tags() const {return tag_map;}
 };
 WRITE_CLASS_ENCODER(RGWObjTags)
