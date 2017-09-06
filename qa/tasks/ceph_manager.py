0b2b39bde85de0d4442241e2e4920762524575b7
Sage Weil <sage@redhat.com>
2017-09-06 13:27:33 -0400

    qa/tasks/ceph_manager: avoid test_map_discontinuity stall with too few in osds
    Some tests have m=2,k=2 and this will break them.
    
    We don't have an easy way from here to see what the min in osds for healthy
    is...  basically this map discontinuity test just sucks.
    
    Signed-off-by: Sage Weil <sage@redhat.com>

--- qa/tasks/ceph_manager.py
+++ qa/tasks/ceph_manager.py
@@ -1018,7 +1018,8 @@ class Thrasher:
                     self.ceph_manager.raw_cluster_cmd('osd', 'reweight',
                                                       str(osd), str(1))
                 if random.uniform(0, 1) < float(
-                        self.config.get('chance_test_map_discontinuity', 0)):
+                        self.config.get('chance_test_map_discontinuity', 0)) \
+                        and len(self.in_osds) > 4:  # kludge: avoid m=2,k=2 stall
                     self.test_map_discontinuity()
                 else:
                     self.ceph_manager.wait_for_recovery(
