charlewn/ceph

osd: use front and back interfaces for heartbeat
Ever since v10.2.0-210-g6f1f717 (which first appeared in kraken), osd
pings are processed by fast_dispatch, and the regular dispatch thread is
not used at all.  That removes any need for or advantage to having
heartbeat messages processed by a different messenger on a different
address than the OSDs main front and back address.  Either way, the
work is being done by the messenger fast dispatch context, which is the
async messenger work queue.  (With SimpleMessenger it is a different
thread, but that's an implementation artifact for fast dispatch.)

If we are connecting to a pre-luminous OSD, keep using the old address.
For luminous or later, us the regular address.

Signed-off-by: Sage Weil <sage@redhat.com>

src/osd/OSD.cc
@@ -971,9 +971,20 @@ pair<ConnectionRef,ConnectionRef> OSDService::get_con_osd_hb(int peer, epoch_t f
     release_map(next_map);
     return ret;
   }
-  ret.first = osd->hb_back_client_messenger->get_connection(next_map->get_hb_back_inst(peer));
-  if (next_map->get_hb_front_addr(peer) != entity_addr_t())
-    ret.second = osd->hb_front_client_messenger->get_connection(next_map->get_hb_front_inst(peer));
+
+  if (HAVE_FEATURE(next_map->get_xinfo(peer).features, SERVER_LUMINOUS)) {
+    ret.first = osd->hb_back_client_messenger->get_connection(
+      next_map->get_cluster_inst(peer));
+    ret.second = osd->hb_front_client_messenger->get_connection(
+      next_map->get_inst(peer));
+  } else {
+    // for jewel compat only
+    ret.first = osd->hb_back_client_messenger->get_connection(
+      next_map->get_hb_back_inst(peer));
+    if (next_map->get_hb_front_addr(peer) != entity_addr_t())
+      ret.second = osd->hb_front_client_messenger->get_connection(
+	next_map->get_hb_front_inst(peer));
+  }
   release_map(next_map);
   return ret;
 }

samchoraria/ceph-ci

src/osd/OSD.cc
@@ -1031,9 +1031,20 @@ pair<ConnectionRef,ConnectionRef> OSDService::get_con_osd_hb(int peer, epoch_t f
     release_map(next_map);
     return ret;
   }
-  ret.first = osd->hb_back_client_messenger->get_connection(next_map->get_hb_back_inst(peer));
-  if (next_map->get_hb_front_addr(peer) != entity_addr_t())
-    ret.second = osd->hb_front_client_messenger->get_connection(next_map->get_hb_front_inst(peer));
+
+  if (HAVE_FEATURE(next_map->get_xinfo(peer).features, SERVER_MIMIC)) {
+    ret.first = osd->hb_back_client_messenger->get_connection(
+      next_map->get_cluster_inst(peer));
+    ret.second = osd->hb_front_client_messenger->get_connection(
+      next_map->get_inst(peer));
+  } else {
+    // for luminous compat only
+    ret.first = osd->hb_back_client_messenger->get_connection(
+      next_map->get_hb_back_inst(peer));
+    if (next_map->get_hb_front_addr(peer) != entity_addr_t())
+      ret.second = osd->hb_front_client_messenger->get_connection(
+	next_map->get_hb_front_inst(peer));
+  }
   release_map(next_map);
   return ret;
 }