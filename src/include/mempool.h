ceph/ceph

commit 2ef222a58c3801eaac5a6d52dda2de1ffe37407b

mds: use mempool for cache objects
The purpose of this is to allow us to track memory usage by cached objects so
we can limit cache size based on memory available/allocated to the MDS.

This commit is a first step: it adds CInode, CDir, and CDentry to the mempool
but not all of the containers in these classes (e.g. std::map). However,
MDSCacheObject has been changed to allocate its containers through the mempool
by converting compact_* containers to the std versions offered through mempool
via the new alloc_ptr.

(A compact_* class simply wraps a pointer to the std:: version to reduce memory
usage of an object when the container is only occasionally used. The alloc_ptr
allows us to achieve the same thing explicitly with only a little handholding:
when all entries in the wrapped container are deleted, the caller must call
alloc_ptr.release().)

Signed-off-by: Patrick Donnelly <pdonnell@redhat.com>
(cherry picked from commit e035b64fcb0482c3318656e1680d683814f494fe)

src/include/mempool.h
@@ -160,6 +160,7 @@ namespace mempool {
   f(osdmap)			      \
   f(osdmap_mapping)		      \
   f(pgmap)			      \
+  f(mds_co)			      \
   f(unittest_1)			      \
   f(unittest_2)
 

src/mds/CDentry.cc
@@ -620,3 +620,5 @@ std::string CDentry::linkage_t::get_remote_d_type_string() const
     default: ceph_abort(); return "";
   }
 }
+
+MEMPOOL_DEFINE_OBJECT_FACTORY(CDentry, co_dentry, mds_co);

src/mds/CDentry.h
@@ -49,6 +49,7 @@ bool operator<(const CDentry& l, const CDentry& r);
 // dentry
 class CDentry : public MDSCacheObject, public LRUObject, public Counter<CDentry> {
 public:
+  MEMPOOL_CLASS_HELPERS();
   friend class CDir;
 
   struct linkage_t {

src/mds/CDir.cc
@@ -3293,3 +3293,4 @@ bool CDir::should_split_fast() const
   return effective_size > fast_limit;
 }
 
+MEMPOOL_DEFINE_OBJECT_FACTORY(CDir, co_dir, mds_co);

src/mds/CDir.h
@@ -46,6 +46,7 @@ class CDir : public MDSCacheObject, public Counter<CDir> {
   friend ostream& operator<<(ostream& out, const class CDir& dir);
 
 public:
+  MEMPOOL_CLASS_HELPERS();
   // -- pins --
   static const int PIN_DNWAITER =     1;
   static const int PIN_INOWAITER =    2;

src/mds/CInode.cc
@@ -4515,3 +4515,5 @@ bool CInode::is_exportable(mds_rank_t dest) const
     return true;
   }
 }
+
+MEMPOOL_DEFINE_OBJECT_FACTORY(CInode, co_inode, mds_co);

src/mds/CInode.h
@@ -130,6 +130,7 @@ WRITE_CLASS_ENCODER_FEATURES(InodeStore)
 // cached inode wrapper
 class CInode : public MDSCacheObject, public InodeStoreBase, public Counter<CInode> {
  public:
+  MEMPOOL_CLASS_HELPERS();
   // -- pins --
   static const int PIN_DIRFRAG =         -1; 
   static const int PIN_CAPS =             2;  // client caps

src/mds/MDCache.cc
@@ -11846,6 +11846,18 @@ void MDCache::show_cache()
   }
 }
 
+int MDCache::cache_status(Formatter *f)
+{
+  f->open_object_section("cache");
+
+  f->open_object_section("pool");
+  mempool::get_pool(mempool::mds_co::id).dump(f);
+  f->close_section();
+
+  f->close_section();
+  return 0;
+}
+
 int MDCache::dump_cache(std::string const &file_name)
 {
   return dump_cache(file_name.c_str(), NULL);

samchoraria/ceph-ci

commit 9a39a8186032bbcf7951f4c033de66807f0e31ab

include/mempool: keep mempool count consistent with 12.2.0
This fixes ABI breakage from 2ef222a58c3801eaac5a6d52dda2de1ffe37407b

Signed-off-by: Sage Weil <sage@redhat.com>

src/include/mempool.h
@@ -161,8 +161,7 @@ namespace mempool {
   f(osdmap_mapping)		      \
   f(pgmap)			      \
   f(mds_co)			      \
-  f(unittest_1)			      \
-  f(unittest_2)
+  f(unittest_1)
 
 
 // give them integer ids