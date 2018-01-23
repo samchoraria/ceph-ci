0e0d7d18587d1148a033fbad11cb9342458ab33d
Abhishek Lekshmanan <abhishek@suse.com>
2018-01-23 16:27:48 +0100

    rgw: policy: refactor has_conditional
    Basically created has_conditional and has_partial_conditional to check for
    exact/partial matches for conditionals and modified exisiting call sites.
    has_key is now a function template that passes on the test string to any given
    function as the second argument.
    
    Backport: removed interactions with object tagging, which isn't on L
    
    Resolves: rhbz#1471626
    
    Signed-off-by: Abhishek Lekshmanan <abhishek@suse.com>
    (cherry picked from commit b2accc222f67a6e14085ca08d1d80e80f749054f)
    Signed-off-by: Matt Benjamin <mbenjamin@redhat.com>

--- src/rgw/rgw_iam_policy.h
+++ src/rgw/rgw_iam_policy.h
@@ -366,6 +366,13 @@ struct Condition {
     }
   };
 
+  struct ci_starts_with {
+    bool operator()(const std::string& s1,
+		    const std::string& s2) const {
+      return boost::istarts_with(s1, s2);
+    }
+  };
+
   template<typename F>
   static bool orrible(F&& f, const std::string& c,
 		      const std::vector<std::string>& v) {
@@ -397,6 +404,12 @@ struct Condition {
     }
     return false;
   }
+
+  template <typename F>
+  bool has_key_p(const std::string& _key, F p) const {
+    return p(key, _key);
+  }
+
 };
 
 std::ostream& operator <<(std::ostream& m, const Condition& c);
@@ -452,6 +465,25 @@ struct Policy {
   Effect eval(const Environment& e,
 	      boost::optional<const rgw::auth::Identity&> ida,
 	      std::uint64_t action, const ARN& resource) const;
+
+  template <typename F>
+  bool has_conditional(const string& conditional, F p) const {
+    for (const auto&s: statements){
+      if (std::any_of(s.conditions.begin(), s.conditions.end(),
+		      [&](const Condition& c) { return c.has_key_p(conditional, p);}))
+	return true;
+    }
+    return false;
+  }
+
+  bool has_conditional(const string& c) const {
+    return has_conditional(c, Condition::ci_equal_to());
+  }
+
+  bool has_partial_conditional(const string& c) const {
+    return has_conditional(c, Condition::ci_starts_with());
+  }
+
 };
 
 std::ostream& operator <<(ostream& m, const Policy& p);
