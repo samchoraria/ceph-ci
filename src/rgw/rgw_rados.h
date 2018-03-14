b8d3aea475d56ffba4822b00146331010901201d
Adam C. Emerson <aemerson@redhat.com>
2018-03-14 16:29:02 -0400

    rgw: Cache notify fault injection
    Allow a configured probability of dropping a notify message on the
    floor without processing or acking it.
    
    Resolves: rhbz#1522881
    
    Signed-off-by: Adam C. Emerson <aemerson@redhat.com>
    (cherry picked from commit a1901f923becfec426744bd55350acac41569379)
    (cherry picked from commit bf1d14e6698f079062da8b656a7cdba56dd52c7f)

--- src/rgw/rgw_rados.h
+++ src/rgw/rgw_rados.h
@@ -1871,6 +1871,8 @@ class RGWRados
   librados::IoCtx control_pool_ctx;   // .rgw.control
   bool watch_initialized;
 
+  double inject_notify_timeout_probability = 0;
+
   friend class RGWWatcher;
 
   Mutex bucket_id_lock;
