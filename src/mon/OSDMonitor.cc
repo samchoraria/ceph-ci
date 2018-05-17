athanatos/ceph

mon/OSDMonitor.cc: fix expected_num_objects interpret error
Fixes: http://tracker.ceph.com/issues/22530
Signed-off-by: Yang Honggang <joseph.yang@xtaotech.com>

src/mon/OSDMonitor.cc
@@ -10154,6 +10154,7 @@ bool OSDMonitor::prepare_command_impl(MonOpRequestRef op,
     }
 
     bool implicit_rule_creation = false;
+    int64_t expected_num_objects = 0;
     string rule_name;
     cmd_getval(cct, cmdmap, "rule", rule_name);
     string erasure_code_profile;
@@ -10191,9 +10192,26 @@ bool OSDMonitor::prepare_command_impl(MonOpRequestRef op,
 	  rule_name = poolstr;
 	}
       }
+      cmd_getval(g_ceph_context, cmdmap, "expected_num_objects",
+                 expected_num_objects, int64_t(0));
     } else {
       //NOTE:for replicated pool,cmd_map will put rule_name to erasure_code_profile field
-      rule_name = erasure_code_profile;
+      //     and put expected_num_objects to rule field
+      if (erasure_code_profile != "") { // cmd is from CLI
+        if (rule_name != "") {
+          string interr;
+          expected_num_objects = strict_strtoll(rule_name.c_str(), 10, &interr);
+          if (interr.length()) {
+            ss << "error parsing integer value '" << rule_name << "': " << interr;
+            err = -EINVAL;
+            goto reply;
+          }
+        }
+        rule_name = erasure_code_profile;
+      } else { // cmd is well-formed
+        cmd_getval(g_ceph_context, cmdmap, "expected_num_objects",
+                   expected_num_objects, int64_t(0));
+      }
     }
 
     if (!implicit_rule_creation && rule_name != "") {
@@ -10207,8 +10225,6 @@ bool OSDMonitor::prepare_command_impl(MonOpRequestRef op,
 	goto reply;
     }
 
-    int64_t expected_num_objects;
-    cmd_getval(cct, cmdmap, "expected_num_objects", expected_num_objects, int64_t(0));
     if (expected_num_objects < 0) {
       ss << "'expected_num_objects' must be non-negative";
       err = -EINVAL;

samchoraria/ceph-ci

(cherry picked from commit c24f2baec968ff66e792991d829591505ffba3ae)
Conflicts:
	qa/standalone/mon/osd-pool-create.sh: jewel does not have
qa/standalone tests by then. and in jewel, "ruleset" is not renamed to
"rule" yet.
	src/mon/OSDMonitor.cc
	src/pybind/mgr/localpool/module.py: trivial resolution
(cherry picked from commit a6af917a8b6f1467f826c18e1a82a7688548a111)
Resolves: rhbz#1554963

src/mon/OSDMonitor.cc
@@ -7029,6 +7029,7 @@ done:
     }
 
     bool implicit_ruleset_creation = false;
+    int64_t expected_num_objects = 0;
     string ruleset_name;
     cmd_getval(g_ceph_context, cmdmap, "ruleset", ruleset_name);
     string erasure_code_profile;
@@ -7066,9 +7067,25 @@ done:
 	  ruleset_name = poolstr;
 	}
       }
+      cmd_getval(g_ceph_context, cmdmap, "expected_num_objects",
+                 expected_num_objects, int64_t(0));
     } else {
       //NOTE:for replicated pool,cmd_map will put ruleset_name to erasure_code_profile field
-      ruleset_name = erasure_code_profile;
+      if (erasure_code_profile != "") { // cmd is from CLI
+        if (ruleset_name != "") {
+          string interr;
+          expected_num_objects = strict_strtoll(ruleset_name.c_str(), 10, &interr);
+          if (interr.length()) {
+            ss << "error parsing integer value '" << ruleset_name << "': " << interr;
+            err = -EINVAL;
+            goto reply;
+          }
+        }
+        ruleset_name = erasure_code_profile;
+      } else { // cmd is well-formed
+        cmd_getval(g_ceph_context, cmdmap, "expected_num_objects",
+                   expected_num_objects, int64_t(0));
+      }
     }
 
     if (!implicit_ruleset_creation && ruleset_name != "") {
@@ -7082,8 +7099,6 @@ done:
 	goto reply;
     }
 
-    int64_t expected_num_objects;
-    cmd_getval(g_ceph_context, cmdmap, "expected_num_objects", expected_num_objects, int64_t(0));
     if (expected_num_objects < 0) {
       ss << "'expected_num_objects' must be non-negative";
       err = -EINVAL;