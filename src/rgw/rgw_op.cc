4d0e14fe0484cb27baac0d810f1b8998ac2cf963
Matt Benjamin <mbenjamin@redhat.com>
2018-06-12 13:14:44 -0400

    rgw: add option for relaxed region enforcement
    Fixes: http://tracker.ceph.com/issues/24507
    Resolves: rhbz#1585307
    
    Signed-off-by: Matt Benjamin <mbenjamin@redhat.com>
    (cherry picked from commit fd0acb28ec092dccc9a086bd4d232f4601d20ddd)

--- src/rgw/rgw_op.cc
+++ src/rgw/rgw_op.cc
@@ -2679,7 +2679,8 @@ void RGWCreateBucket::execute()
   if (op_ret < 0)
     return;
 
-  if (!location_constraint.empty() &&
+  if (!relaxed_region_enforcement &&
+      !location_constraint.empty() &&
       !store->has_zonegroup_api(location_constraint)) {
       ldout(s->cct, 0) << "location constraint (" << location_constraint << ")"
                        << " can't be found." << dendl;
@@ -2688,7 +2689,7 @@ void RGWCreateBucket::execute()
       return;
   }
 
-  if (!store->get_zonegroup().is_master_zonegroup() && !location_constraint.empty() &&
+  if (!relaxed_region_enforcement && !store->get_zonegroup().is_master_zonegroup() && !location_constraint.empty() &&
       store->get_zonegroup().api_name != location_constraint) {
     ldout(s->cct, 0) << "location constraint (" << location_constraint << ")"
                      << " doesn't match zonegroup" << " (" << store->get_zonegroup().api_name << ")"
