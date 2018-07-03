5f91ee632644287e91e6b5a859e76d23060dd8a1
Ricardo Dias <rdias@suse.com>
2018-07-03 11:34:11 +0100

    mgr/dashboard: frontend: JWT authentication implementation
    Signed-off-by: Ricardo Dias <rdias@suse.com>

--- src/pybind/mgr/dashboard/frontend/src/app/app.module.ts
+++ src/pybind/mgr/dashboard/frontend/src/app/app.module.ts
@@ -3,9 +3,10 @@ import { ErrorHandler, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import { JwtModule } from '@auth0/angular-jwt';
 import { ToastModule, ToastOptions } from 'ng2-toastr/ng2-toastr';
-
 import { AccordionModule, BsDropdownModule, TabsModule } from 'ngx-bootstrap';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CephModule } from './ceph/ceph.module';
@@ -21,6 +22,10 @@ export class CustomOption extends ToastOptions {
   enableHTML = true;
 }
 
+export function jwtTokenGetter() {
+  return localStorage.getItem('access_token');
+}
+
 @NgModule({
   declarations: [AppComponent],
   imports: [
@@ -34,7 +39,12 @@ export class CustomOption extends ToastOptions {
     CephModule,
     AccordionModule.forRoot(),
     BsDropdownModule.forRoot(),
-    TabsModule.forRoot()
+    TabsModule.forRoot(),
+    JwtModule.forRoot({
+      config: {
+        tokenGetter: jwtTokenGetter
+      }
+    })
   ],
   exports: [SharedModule],
   providers: [
