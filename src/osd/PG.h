athanatos/ceph

osd/PG: move send_notify() into RecoveryCtx
Signed-off-by: Sage Weil <sage@redhat.com>

src/osd/PG.h
@@ -1033,6 +1033,12 @@ public:
 	ovec.insert(ovec.end(), i->second.begin(), i->second.end());
       }
     }
+
+    void send_notify(pg_shard_t to,
+		     const pg_notify_t &info, const PastIntervals &pi) {
+      assert(notify_list);
+      (*notify_list)[to.osd].push_back(make_pair(info, pi));
+    }
   };
 protected:
 
@@ -1994,8 +2000,7 @@ protected:
       void send_notify(pg_shard_t to,
 		       const pg_notify_t &info, const PastIntervals &pi) {
 	assert(state->rctx);
-	assert(state->rctx->notify_list);
-	(*state->rctx->notify_list)[to.osd].push_back(make_pair(info, pi));
+	state->rctx->send_notify(to, info, pi);
       }
     };
     friend class RecoveryMachine;

samchoraria/ceph-ci

(cherry picked from commit 856fd0243346ca99fa4a3ba5014a7bcdf2c29450)
Conflicts:
    src/osd/PG.h - renamed PastInterval type
Resolves: rhbz#1595283

src/osd/PG.h
@@ -668,6 +668,12 @@ public:
 	ovec.insert(ovec.end(), i->second.begin(), i->second.end());
       }
     }
+
+    void send_notify(pg_shard_t to,
+		     const pg_notify_t &info, const pg_interval_map_t &pi) {
+      assert(notify_list);
+      (*notify_list)[to.osd].push_back(make_pair(info, pi));
+    }
   };
 
   struct NamedState {
@@ -1636,8 +1642,7 @@ public:
       void send_notify(pg_shard_t to,
 		       const pg_notify_t &info, const pg_interval_map_t &pi) {
 	assert(state->rctx);
-	assert(state->rctx->notify_list);
-	(*state->rctx->notify_list)[to.osd].push_back(make_pair(info, pi));
+	state->rctx->send_notify(to, info, pi);
       }
     };
     friend class RecoveryMachine;