ricardoasmarques/ceph

mgr/dashboard: SAML 2.0 support
Signed-off-by: Ricardo Dias <rdias@suse.com>
Signed-off-by: Ricardo Marques <rimarques@suse.com>

src/pybind/mgr/dashboard/controllers/auth.py
@@ -9,6 +9,8 @@ from . import ApiController, RESTController
 from .. import logger
 from ..exceptions import DashboardException
 from ..services.auth import AuthManager
+from ..services.access_control import ACCESS_CTRL_DB
+from ..services.sso import SSO_DB
 from ..tools import Session
 
 
@@ -44,7 +46,31 @@ class Auth(RESTController):
                                  code='invalid_credentials',
                                  component='auth')
 
-    def bulk_delete(self):
+    @RESTController.Collection('POST')
+    def logout(self):
         logger.debug('Logout successful')
         cherrypy.session[Session.USERNAME] = None
         cherrypy.session[Session.TS] = None
+        redirect_url = '#/login'
+        if SSO_DB.protocol == 'saml2':
+            redirect_url = 'auth/saml/slo'
+        return {
+            'redirect_url': redirect_url
+        }
+
+    def _get_login_url(self):
+        if SSO_DB.protocol == 'saml2':
+            return 'auth/saml/login'
+        return '#/login'
+
+    def list(self):
+        if not cherrypy.session.get(Session.USERNAME):
+            return {
+                'login_url': self._get_login_url()
+            }
+        # TODO : check if authenticated user has been removed
+        user = ACCESS_CTRL_DB.get_user(cherrypy.session.get(Session.USERNAME))
+        return {
+            'username': user.username,
+            'permissions': user.permissions_dict(),
+        }

samchoraria/ceph-ci

Fixes: https://tracker.ceph.com/issues/24268

src/pybind/mgr/dashboard/controllers/auth.py
@@ -9,6 +9,8 @@ from . import ApiController, RESTController
 from .. import logger
 from ..exceptions import DashboardException
 from ..services.auth import AuthManager
+from ..services.access_control import ACCESS_CTRL_DB
+from ..services.sso import SSO_DB
 from ..tools import Session
 
 
@@ -44,7 +46,30 @@ class Auth(RESTController):
                                  code='invalid_credentials',
                                  component='auth')
 
-    def bulk_delete(self):
+    @RESTController.Collection('POST')
+    def logout(self):
         logger.debug('Logout successful')
         cherrypy.session[Session.USERNAME] = None
         cherrypy.session[Session.TS] = None
+        redirect_url = '#/login'
+        if SSO_DB.protocol == 'saml2':
+            redirect_url = 'auth/saml2/slo'
+        return {
+            'redirect_url': redirect_url
+        }
+
+    def _get_login_url(self):
+        if SSO_DB.protocol == 'saml2':
+            return 'auth/saml2/login'
+        return '#/login'
+
+    def list(self):
+        if not cherrypy.session.get(Session.USERNAME):
+            return {
+                'login_url': self._get_login_url()
+            }
+        user = ACCESS_CTRL_DB.get_user(cherrypy.session.get(Session.USERNAME))
+        return {
+            'username': user.username,
+            'permissions': user.permissions_dict(),
+        }