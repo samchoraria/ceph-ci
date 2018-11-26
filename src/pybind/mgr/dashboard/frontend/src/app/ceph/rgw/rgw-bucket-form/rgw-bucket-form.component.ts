ceph/ceph

commit f21d0da5a3e19e3f9bbde084e71b4a09f8dcb0a1

mgr/dashboard: Add support for URI encode
Created a decorator and pipe to help encode special URI components in the
frontend.

Modified the backend request handler to decode all the string args.

fixes: http://tracker.ceph.com/issues/24621

Signed-off-by: Tiago Melo <tmelo@suse.com>

src/pybind/mgr/dashboard/frontend/src/app/ceph/rgw/rgw-bucket-form/rgw-bucket-form.component.ts
@@ -56,6 +56,7 @@ export class RgwBucketFormComponent implements OnInit {
         if (!params.hasOwnProperty('bucket')) {
           return;
         }
+        params.bucket = decodeURIComponent(params.bucket);
         this.loading = true;
         // Load the bucket data in 'edit' mode.
         this.editing = true;

qa/tasks/mgr/dashboard/test_rbd.py
@@ -580,3 +580,22 @@ class RbdTest(DashboardTestCase):
         self.assertEqual(default_features, ['deep-flatten', 'exclusive-lock',
                                              'fast-diff', 'layering',
                                              'object-map'])
+
+    def test_image_with_special_name(self):
+        rbd_name = 'test/rbd'
+        rbd_name_encoded = 'test%2Frbd'
+
+        self.create_image('rbd', rbd_name, 10240)
+        self.assertStatus(201)
+
+        img = self._get("/api/block/image/rbd/" + rbd_name_encoded)
+        self.assertStatus(200)
+
+        self._validate_image(img, name=rbd_name, size=10240,
+                             num_objs=1, obj_size=4194304,
+                             features_name=['deep-flatten',
+                                            'exclusive-lock',
+                                            'fast-diff', 'layering',
+                                            'object-map'])
+
+        self.remove_image('rbd', rbd_name_encoded)

src/pybind/mgr/dashboard/controllers/__init__.py
@@ -9,9 +9,15 @@ import json
 import os
 import pkgutil
 import sys
-
-import cherrypy
 from six import add_metaclass
+
+if sys.version_info >= (3, 0):
+    from urllib.parse import unquote  # pylint: disable=no-name-in-module,import-error
+else:
+    from urllib import unquote  # pylint: disable=no-name-in-module
+
+# pylint: disable=wrong-import-position
+import cherrypy
 
 from .. import logger
 from ..security import Scope, Permission
@@ -521,6 +527,12 @@ class BaseController(object):
     def _request_wrapper(func, method, json_response):
         @wraps(func)
         def inner(*args, **kwargs):
+            for key, value in kwargs.items():
+                # pylint: disable=undefined-variable
+                if (sys.version_info < (3, 0) and isinstance(value, unicode)) \
+                        or isinstance(value, str):
+                    kwargs[key] = unquote(value)
+
             if method in ['GET', 'DELETE']:
                 ret = func(*args, **kwargs)
 

src/pybind/mgr/dashboard/frontend/src/app/ceph/block/rbd-form/rbd-form.component.ts
@@ -206,9 +206,9 @@ export class RbdFormComponent implements OnInit {
       this.mode === this.rbdFormMode.copying
     ) {
       this.route.params.subscribe((params: { pool: string; name: string; snap: string }) => {
-        const poolName = params.pool;
-        const rbdName = params.name;
-        this.snapName = params.snap;
+        const poolName = decodeURIComponent(params.pool);
+        const rbdName = decodeURIComponent(params.name);
+        this.snapName = decodeURIComponent(params.snap);
         this.rbdService.get(poolName, rbdName).subscribe((resp: RbdFormResponseModel) => {
           this.setResponse(resp, this.snapName);
         });

src/pybind/mgr/dashboard/frontend/src/app/ceph/block/rbd-list/rbd-list.component.html
@@ -32,8 +32,9 @@
               class="btn btn-sm btn-primary"
               *ngIf="permission.update && (!permission.create || permission.create && selection.hasSingleSelection)"
               [ngClass]="{'disabled': !selection.hasSingleSelection || selection.first().executing}"
-              routerLink="/rbd/edit/{{ selection.first()?.pool_name }}/{{ selection.first()?.name }}">
-        <i class="fa fa-fw fa-pencil"></i><span i18n>Edit</span>
+              routerLink="/rbd/edit/{{ selection.first()?.pool_name | encodeUri }}/{{ selection.first()?.name | encodeUri }}">
+        <i class="fa fa-fw fa-pencil"></i>
+        <span i18n>Edit</span>
       </button>
       <button type="button"
               class="btn btn-sm btn-primary"
@@ -64,7 +65,7 @@
             *ngIf="permission.update"
             [ngClass]="{'disabled': !selection.hasSingleSelection || selection.first().executing}">
           <a class="dropdown-item"
-             routerLink="/rbd/edit/{{ selection.first()?.pool_name }}/{{ selection.first()?.name }}">
+             routerLink="/rbd/edit/{{ selection.first()?.pool_name | encodeUri }}/{{ selection.first()?.name | encodeUri }}">
             <i class="fa fa-fw fa-pencil"></i>
             <span i18n>Edit</span>
           </a>
@@ -73,7 +74,7 @@
             *ngIf="permission.create"
             [ngClass]="{'disabled': !selection.hasSingleSelection || selection.first().executing}">
           <a class="dropdown-item"
-             routerLink="/rbd/copy/{{ selection.first()?.pool_name }}/{{ selection.first()?.name }}">
+             routerLink="/rbd/copy/{{ selection.first()?.pool_name | encodeUri }}/{{ selection.first()?.name | encodeUri }}">
             <i class="fa fa-fw fa-copy"></i>
             <span i18n>Copy</span>
           </a>

src/pybind/mgr/dashboard/frontend/src/app/ceph/block/rbd-snapshot-list/rbd-snapshot-list.component.html
@@ -73,7 +73,7 @@
             *ngIf="permission.create"
             [ngClass]="{'disabled': !selection.hasSingleSelection || selection.first().executing}">
           <a class="dropdown-item"
-             routerLink="/rbd/clone/{{ poolName }}/{{ rbdName }}/{{ selection.first()?.name }}">
+             routerLink="/rbd/clone/{{ poolName | encodeUri }}/{{ rbdName | encodeUri }}/{{ selection.first()?.name | encodeUri }}">
             <i class="fa fa-fw fa-clone"></i>
             <span i18n>Clone</span>
           </a>
@@ -81,8 +81,10 @@
         <li role="menuitem"
             *ngIf="permission.create"
             [ngClass]="{'disabled': !selection.hasSingleSelection || selection.first().executing}">
-          <a class="dropdown-item" routerLink="/rbd/copy/{{ poolName }}/{{ rbdName }}/{{ selection.first()?.name }}">
-            <i class="fa fa-fw fa-copy"></i><span i18n>Copy</span>
+          <a class="dropdown-item"
+             routerLink="/rbd/copy/{{ poolName | encodeUri }}/{{ rbdName | encodeUri }}/{{ selection.first()?.name | encodeUri }}">
+            <i class="fa fa-fw fa-copy"></i>
+            <span i18n>Copy</span>
           </a>
         </li>
         <li role="menuitem"

src/pybind/mgr/dashboard/frontend/src/app/ceph/block/rbd-snapshot-list/rbd-snapshot-list.component.spec.ts
@@ -11,6 +11,7 @@ import { RbdService } from '../../../shared/api/rbd.service';
 import { ComponentsModule } from '../../../shared/components/components.module';
 import { DataTableModule } from '../../../shared/datatable/datatable.module';
 import { Permissions } from '../../../shared/models/permissions';
+import { PipesModule } from '../../../shared/pipes/pipes.module';
 import { AuthStorageService } from '../../../shared/services/auth-storage.service';
 import { NotificationService } from '../../../shared/services/notification.service';
 import { ServicesModule } from '../../../shared/services/services.module';
@@ -40,7 +41,8 @@ describe('RbdSnapshotListComponent', () => {
       ServicesModule,
       ApiModule,
       HttpClientTestingModule,
-      RouterTestingModule
+      RouterTestingModule,
+      PipesModule
     ],
     providers: [{ provide: AuthStorageService, useValue: fakeAuthStorageService }]
   });

src/pybind/mgr/dashboard/frontend/src/app/ceph/rgw/rgw-bucket-list/rgw-bucket-list.component.html
@@ -34,7 +34,7 @@
               class="btn btn-sm btn-primary"
               [ngClass]="{'disabled': !selection.hasSelection}"
               *ngIf="permission.update && (!permission.create && !selection.hasMultiSelection || selection.hasSingleSelection)"
-              routerLink="/rgw/bucket/edit/{{ selection.first()?.bucket }}">
+              routerLink="/rgw/bucket/edit/{{ selection.first()?.bucket | encodeUri }}">
         <i class="fa fa-fw fa-pencil"></i>
         <ng-container i18n>Edit</ng-container>
       </button>
@@ -69,7 +69,7 @@
             *ngIf="permission.update"
             [ngClass]="{'disabled': !selection.hasSingleSelection}">
           <a class="dropdown-item"
-             routerLink="/rgw/bucket/edit/{{ selection.first()?.bucket }}"
+             routerLink="/rgw/bucket/edit/{{ selection.first()?.bucket | encodeUri }}"
              i18n>
             <i class="fa fa-fw fa-pencil"></i>
             Edit

samchoraria/ceph-ci

commit 1ffb12fcbcf05a546960a70e010a8070194c0f0b

mgr/dashboard: Fix RGW bucket edit issue
Commit f21d0da5a3e19e3f9bbde084e71b4a09f8dcb0a1 introduced an error in
bucket edit action.

Fixes: https://bugzilla.redhat.com/show_bug.cgi?id=1647154
RHCS: 3.2
Signed-off-by: Ernesto Puerta <epuertat@redhat.com>

src/pybind/mgr/dashboard/frontend/src/app/ceph/rgw/rgw-bucket-form/rgw-bucket-form.component.ts
@@ -51,11 +51,11 @@ export class RgwBucketFormComponent implements OnInit {
         if (!params.hasOwnProperty('bucket')) {
           return;
         }
-        params.bucket = decodeURIComponent(params.bucket);
+        const bucket = decodeURIComponent(params.bucket);
         this.loading = true;
         // Load the bucket data in 'edit' mode.
         this.editing = true;
-        this.rgwBucketService.get(params.bucket).subscribe((resp: object) => {
+        this.rgwBucketService.get(bucket).subscribe((resp: object) => {
           this.loading = false;
           // Get the default values.
           const defaults = _.clone(this.bucketForm.value);

src/pybind/mgr/dashboard/frontend/src/app/ceph/rgw/rgw-bucket-list/rgw-bucket-list.component.ts
@@ -47,7 +47,7 @@ export class RgwBucketListComponent {
       }
     ];
     const getBucketUri = () =>
-      this.selection.first() && `${encodeURI(this.selection.first().bucket)}`;
+      this.selection.first() && `${encodeURIComponent(this.selection.first().bucket)}`;
     const addAction: CdTableAction = {
       permission: 'create',
       icon: 'fa-plus',

src/pybind/mgr/dashboard/frontend/src/app/ceph/rgw/rgw-user-form/rgw-user-form.component.ts
@@ -154,13 +154,14 @@ export class RgwUserFormComponent implements OnInit {
       if (!params.hasOwnProperty('uid')) {
         return;
       }
+      const uid = decodeURIComponent(params.uid);
       this.loading = true;
       // Load the user data in 'edit' mode.
       this.editing = true;
       // Load the user and quota information.
       const observables = [];
-      observables.push(this.rgwUserService.get(params.uid));
-      observables.push(this.rgwUserService.getQuota(params.uid));
+      observables.push(this.rgwUserService.get(uid));
+      observables.push(this.rgwUserService.getQuota(uid));
       observableForkJoin(observables).subscribe(
         (resp: any[]) => {
           this.loading = false;

src/pybind/mgr/dashboard/frontend/src/app/ceph/rgw/rgw-user-list/rgw-user-list.component.ts
@@ -63,7 +63,8 @@ export class RgwUserListComponent {
         flexGrow: 1
       }
     ];
-    const getUserUri = () => this.selection.first() && this.selection.first().user_id;
+    const getUserUri = () =>
+      this.selection.first() && `${encodeURIComponent(this.selection.first().user_id)}`;
     const addAction: CdTableAction = {
       permission: 'create',
       icon: 'fa-plus',