rzarzynski/ceph

common: simplify the C-string-taking variant of bl::append().
Signed-off-by: Radoslaw Zarzynski <rzarzyns@redhat.com>

src/include/buffer.h
@@ -694,6 +694,7 @@ namespace buffer CEPH_BUFFER_API {
   private:
     mutable iterator last_p;
     int zero_copy_to_fd(int fd) const;
+    ptr& refill_append_space(const unsigned len);
 
   public:
     // cons/des

samchoraria/ceph-ci

src/include/buffer.h
@@ -977,6 +977,8 @@ namespace buffer CEPH_BUFFER_API {
   private:
     mutable iterator last_p;
 
+    ptr_node& refill_append_space(const unsigned len);
+
   public:
     // cons/des
     list() : _len(0), _memcopy_count(0), last_p(this) {}