0559b96b63a74ceec3e90922038d94c2d7e76485
Ali Maredia <amaredia@redhat.com>
2019-01-07 15:01:34 -0500

    qa: test boto3 tests without encryption tests
    Signed-off-by: Ali Maredia <amaredia@redhat.com>

--- qa/tasks/s3tests.py
+++ qa/tasks/s3tests.py
@@ -249,7 +249,7 @@ def run_tests(ctx, config):
     assert isinstance(config, dict)
     testdir = teuthology.get_testdir(ctx)
     # civetweb > 1.8 && beast parsers are strict on rfc2616
-    attrs = ["!fails_on_rgw", "!lifecycle_expiration", "!fails_strict_rfc2616"]
+    attrs = ["!fails_on_rgw", "!lifecycle_expiration", "!fails_strict_rfc2616", "!encryption"]
     for client, client_config in config.iteritems():
         (remote,) = ctx.cluster.only(client).remotes.keys()
         args = [
