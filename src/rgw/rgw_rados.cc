477e32241c983c68beb6e0b1179e52eee6ede8e5
J. Eric Ivancich <ivancich@redhat.com>
2019-04-01 10:59:51 -0400

    rgw: fix race b/w bucket reshard and ops waiting on reshard completion
    A previous commit (f84f70d4) added functionality to clean up old
    bucket instances and bucket shards once sharding completed
    successfully. However the existing code that allowed a bucket
    operation to wait until resharding completed was relying on a field in
    the old bucket instance to know the bucket instance id of the new
    bucket instance. This created a race condition as to whether the
    clean-up or read of the bucket instance id would occur first.
    
    This solution rereads the bucket entry point object when resharding
    completes to determine the bucket instance id of the new bucket
    thereby avoiding the race.
    
    Signed-off-by: J. Eric Ivancich <ivancich@redhat.com>
    (cherry picked from commit 26b793680805721cac86f64cdd234b273bf33be4)
    
    Conflicts:
            src/rgw/rgw_rados.cc: equivalent changes made to rgw_reshard.cc
    
    Resolves: rhbz#1688378

--- src/rgw/rgw_rados.cc
+++ src/rgw/rgw_rados.cc
@@ -5627,6 +5627,7 @@ int RGWRados::Bucket::update_bucket_id(const string& new_bucket_id)
 
   RGWObjectCtx obj_ctx(store);
 
+  bucket_info.objv_tracker.clear();
   int ret = store->get_bucket_instance_info(obj_ctx, bucket, bucket_info, nullptr, nullptr);
   if (ret < 0) {
     return ret;
@@ -10365,7 +10366,7 @@ int RGWRados::Bucket::UpdateIndex::guard_reshard(BucketShard **pbs, std::functio
       return r;
     }
     invalidate_bs();
-  }
+  } // for loop
 
   if (r < 0) {
     return r;
@@ -11344,7 +11345,7 @@ int RGWRados::guard_reshard(BucketShard *bs,
     obj = *pobj;
     obj.bucket.update_bucket_id(new_bucket_id);
     pobj = &obj;
-  }
+  } // for loop
 
   if (r < 0) {
     return r;
