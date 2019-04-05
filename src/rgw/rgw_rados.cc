ceph/ceph

rgw: add perf counters to fetch_remote_obj()
Signed-off-by: Casey Bodley <cbodley@redhat.com>

src/rgw/rgw_rados.cc
@@ -4280,7 +4280,8 @@ int RGWRados::fetch_remote_obj(RGWObjectCtx& obj_ctx,
                string *petag,
                void (*progress_cb)(off_t, void *),
                void *progress_data,
-               rgw_zone_set *zones_trace)
+               rgw_zone_set *zones_trace,
+               std::optional<uint64_t>* bytes_transferred)
 {
   /* source is in a different zonegroup, copy from there */
 
@@ -4494,6 +4495,9 @@ int RGWRados::fetch_remote_obj(RGWObjectCtx& obj_ctx,
     goto set_err_state;
   }
 
+  if (bytes_transferred) {
+    *bytes_transferred = cb.get_data_len();
+  }
   return 0;
 set_err_state:
   if (copy_if_newer && ret == -ERR_NOT_MODIFIED) {

src/rgw/rgw_rados.h
@@ -1919,7 +1919,8 @@ public:
                        string *petag,
                        void (*progress_cb)(off_t, void *),
                        void *progress_data,
-                       rgw_zone_set *zones_trace= nullptr);
+                       rgw_zone_set *zones_trace= nullptr,
+                       std::optional<uint64_t>* bytes_transferred = 0);
   /**
    * Copy an object.
    * dest_obj: the object to copy into

samchoraria/ceph-ci

(cherry picked from commit 5fcb59060bb03c532e0cf03b32352b046c3f9543)
Conflicts:
	src/rgw/rgw_cr_rados.cc - std::optional -> boost
	src/rgw/rgw_cr_rados.h
	src/rgw/rgw_data_sync.cc
	src/rgw/rgw_rados.cc
Resolves: rhbz#1684642

src/rgw/rgw_rados.cc
@@ -8044,7 +8044,8 @@ int RGWRados::fetch_remote_obj(RGWObjectCtx& obj_ctx,
                ceph::buffer::list *petag,
                void (*progress_cb)(off_t, void *),
                void *progress_data,
-               rgw_zone_set *zones_trace)
+               rgw_zone_set *zones_trace,
+               boost::optional<uint64_t>* bytes_transferred)
 {
   /* source is in a different zonegroup, copy from there */
 
@@ -8260,6 +8261,9 @@ int RGWRados::fetch_remote_obj(RGWObjectCtx& obj_ctx,
     delete opstate;
   }
 
+  if (bytes_transferred) {
+    *bytes_transferred = cb.get_data_len();
+  }
   return 0;
 set_err_state:
   if (copy_if_newer && ret == -ERR_NOT_MODIFIED) {

src/rgw/rgw_rados.h
@@ -3174,7 +3174,8 @@ public:
                        ceph::buffer::list *petag,
                        void (*progress_cb)(off_t, void *),
                        void *progress_data,
-                       rgw_zone_set *zones_trace= nullptr);
+                       rgw_zone_set *zones_trace= nullptr,
+                       boost::optional<uint64_t>* bytes_transferred = 0);
   /**
    * Copy an object.
    * dest_obj: the object to copy into