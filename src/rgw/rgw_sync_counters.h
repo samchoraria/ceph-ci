ceph/ceph

rgw: add perf counter definitions for multisite sync
Fixes: http://tracker.ceph.com/issues/38549

Signed-off-by: Casey Bodley <cbodley@redhat.com>

src/rgw/rgw_sync_counters.h
@@ -1,0 +1,25 @@
+// -*- mode:C++; tab-width:8; c-basic-offset:2; indent-tabs-mode:t -*-
+// vim: ts=8 sw=2 smarttab
+
+#pragma once
+
+#include "common/perf_counters_collection.h"
+
+namespace sync_counters {
+
+enum {
+  l_first = 805000,
+
+  l_fetch,
+  l_fetch_not_modified,
+  l_fetch_err,
+
+  l_poll,
+  l_poll_err,
+
+  l_last,
+};
+
+PerfCountersRef build(CephContext *cct, const std::string& name);
+
+} // namespace sync_counters

samchoraria/ceph-ci

(cherry picked from commit d611fe9954d234ae15345cc99eb705ecbb03964f)
Conflicts: no PerfCountersRef
Resolves: rhbz#1684642

src/rgw/rgw_sync_counters.h
@@ -1,0 +1,25 @@
+// -*- mode:C++; tab-width:8; c-basic-offset:2; indent-tabs-mode:t -*-
+// vim: ts=8 sw=2 smarttab
+
+#pragma once
+
+#include "common/perf_counters.h"
+
+namespace sync_counters {
+
+enum {
+  l_first = 805000,
+
+  l_fetch,
+  l_fetch_not_modified,
+  l_fetch_err,
+
+  l_poll,
+  l_poll_err,
+
+  l_last,
+};
+
+PerfCounters* build(CephContext *cct, const std::string& name);
+
+} // namespace sync_counters