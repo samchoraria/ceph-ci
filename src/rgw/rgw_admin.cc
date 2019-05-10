ceph/ceph

Set object ACLS to reflect bucket owner supporting bucket unlink/link.
Provides command line tool to update the acl on object
of a bucket after bucket unlink/link.
"radosgw-admin bucket chown --bucket <bucket>"

Signed-off-by: Shilpa Jagannath <smanjara@redhat.com>

src/rgw/rgw_admin.cc
@@ -102,6 +102,7 @@ void usage()
   cout << "  bucket stats               returns bucket statistics\n";
   cout << "  bucket rm                  remove bucket\n";
   cout << "  bucket check               check bucket index\n";
+  cout << "  bucket chown               update object owner ACL after bucket link\n";
   cout << "  bucket reshard             reshard bucket\n";
   cout << "  bucket rewrite             rewrite all objects in the specified bucket\n";
   cout << "  bucket sync disable        disable bucket sync\n";
@@ -407,6 +408,7 @@ enum {
   OPT_BUCKET_RM,
   OPT_BUCKET_REWRITE,
   OPT_BUCKET_RESHARD,
+  OPT_BUCKET_CHOWN,
   OPT_POLICY,
   OPT_POOL_ADD,
   OPT_POOL_RM,
@@ -669,6 +671,8 @@ static int get_cmd(const char *cmd, const char *prev_cmd, const char *prev_prev_
       return OPT_BUCKET_STATS;
     if (strcmp(cmd, "rm") == 0)
       return OPT_BUCKET_RM;
+    if (strcmp(cmd, "chown") == 0)
+      return OPT_BUCKET_CHOWN;
     if (strcmp(cmd, "rewrite") == 0)
       return OPT_BUCKET_REWRITE;
     if (strcmp(cmd, "reshard") == 0)
@@ -5503,6 +5507,86 @@ int main(int argc, const char **argv)
       return -r;
     }
   }
+
+  if (opt_cmd == OPT_BUCKET_CHOWN) {
+    int ret = 0;
+    std::string tenant;
+    std::vector<rgw_bucket_dir_entry> objs;
+    map<string, bool> common_prefixes;
+    RGWObjectCtx obj_ctx(store);
+
+    //Get bucket info
+    RGWBucketInfo bucket_info;
+    RGWSysObjectCtx sys_ctx = store->svc.sysobj->init_obj_ctx();
+    map<string, bufferlist> attrs;
+
+    //split tenant/bucket
+    auto pos = bucket_name.find('/');
+    if (pos != boost::string_ref::npos) {
+      tenant = bucket_name.substr(0, pos);
+      bucket_name = bucket_name.substr(pos + 1);
+    }
+
+    ret = store->get_bucket_info(sys_ctx, tenant, bucket_name, bucket_info, NULL, &attrs);
+    if (ret < 0) {
+      cerr << " Bucket info failed: tenant: "<< tenant << "bucket_name: " << bucket_name << " " << ret << std::endl;
+      return ret;
+    }
+
+    //Get User info - for display name
+    RGWUserInfo user_info;
+    ret = rgw_get_user_info_by_uid(store, bucket_info.owner, user_info);
+    if (ret < 0) {
+      cerr << " User info failed: "<< ret << std::endl;
+      return ret;
+    }
+
+    RGWRados::Bucket target(store, bucket_info);
+    RGWRados::Bucket::List list_op(&target);
+    int max = 1000;
+
+    list_op.params.list_versions = true;
+    list_op.params.allow_unordered = true;
+
+    bool is_truncated = false;
+
+    do {
+      objs.clear();
+      ret = list_op.list_objects(max, &objs, &common_prefixes, &is_truncated);
+      if (ret < 0) {
+        cerr << " List objects failed: "<< ret << std::endl;
+        return ret;
+      }
+      //Loop through the results
+      for (const auto& obj : objs) {
+        const auto& aiter = attrs.find(RGW_ATTR_ACL);
+        bufferlist& bl = aiter->second;
+        RGWAccessControlPolicy policy;
+        ACLOwner owner;
+        try {
+          decode(policy, bl);
+          owner = policy.get_owner();
+        } catch (buffer::error& err) {
+          cerr << " Decode policy failed: " << std::endl;
+          return -EIO;
+        }
+
+        owner.set_id(bucket_info.owner);
+        owner.set_name(user_info.display_name);
+        policy.set_owner(owner);
+
+        bl.clear();
+        encode(policy, bl);
+
+        const rgw_obj r_obj(bucket_info.bucket, obj.key);
+        ret = modify_obj_attr(store, obj_ctx, bucket_info, r_obj, RGW_ATTR_ACL, bl);
+        if (ret < 0) {
+          cerr << " Modify attr failed: "<< ret << std::endl;
+          return ret;
+        }
+      }// for loop
+    } while(is_truncated);
+  } // if
 
   if (opt_cmd == OPT_LOG_LIST) {
     // filter by date?

samchoraria/ceph-ci

Resolves: rhbz#1669901
(cherry picked from commit abc0f1737181ef1af468f3ade203c5d37c5c9154)
Signed-off-by: Matt Benjamin <mbenjamin@redhat.com>

src/rgw/rgw_admin.cc
@@ -85,6 +85,7 @@ void usage()
   cout << "  bucket stats               returns bucket statistics\n";
   cout << "  bucket rm                  remove bucket\n";
   cout << "  bucket check               check bucket index\n";
+  cout << "  bucket chown               update object owner ACL after bucket link\n";
   cout << "  bucket reshard             reshard bucket\n";
   cout << "  bucket sync disable        disable bucket sync\n";
   cout << "  bucket sync enable         enable bucket sync\n";
@@ -376,6 +377,7 @@ enum {
   OPT_BUCKET_RM,
   OPT_BUCKET_REWRITE,
   OPT_BUCKET_RESHARD,
+  OPT_BUCKET_CHOWN,
   OPT_POLICY,
   OPT_POOL_ADD,
   OPT_POOL_RM,
@@ -620,6 +622,8 @@ static int get_cmd(const char *cmd, const char *prev_cmd, const char *prev_prev_
       return OPT_BUCKET_STATS;
     if (strcmp(cmd, "rm") == 0)
       return OPT_BUCKET_RM;
+    if (strcmp(cmd, "chown") == 0)
+      return OPT_BUCKET_CHOWN;
     if (strcmp(cmd, "rewrite") == 0)
       return OPT_BUCKET_REWRITE;
     if (strcmp(cmd, "reshard") == 0)
@@ -5302,6 +5306,85 @@ int main(int argc, const char **argv)
       return -r;
     }
   }
+
+  if (opt_cmd == OPT_BUCKET_CHOWN) {
+    int ret = 0;
+    std::string tenant;
+    std::vector<rgw_bucket_dir_entry> objs;
+    map<string, bool> common_prefixes;
+    RGWObjectCtx obj_ctx(store);
+
+    //Get bucket info
+    RGWBucketInfo bucket_info;
+    map<string, bufferlist> attrs;
+
+    //split tenant/bucket
+    auto pos = bucket_name.find('/');
+    if (pos != boost::string_ref::npos) {
+      tenant = bucket_name.substr(0, pos);
+      bucket_name = bucket_name.substr(pos + 1);
+    }
+
+    ret = store->get_bucket_info(obj_ctx, tenant, bucket_name, bucket_info, NULL, &attrs);
+    if (ret < 0) {
+      cerr << " Bucket info failed: tenant: "<< tenant << "bucket_name: " << bucket_name << " " << ret << std::endl;
+      return ret;
+    }
+
+    //Get User info - for display name
+    RGWUserInfo user_info;
+    ret = rgw_get_user_info_by_uid(store, bucket_info.owner, user_info);
+    if (ret < 0) {
+      cerr << " User info failed: "<< ret << std::endl;
+      return ret;
+    }
+
+    RGWRados::Bucket target(store, bucket_info);
+    RGWRados::Bucket::List list_op(&target);
+    int max = 1000;
+
+    list_op.params.list_versions = true;
+    list_op.params.allow_unordered = true;
+
+    bool is_truncated = false;
+
+    do {
+      objs.clear();
+      ret = list_op.list_objects(max, &objs, &common_prefixes, &is_truncated);
+      if (ret < 0) {
+        cerr << " List objects failed: "<< ret << std::endl;
+        return ret;
+      }
+      //Loop through the results
+      for (const auto& obj : objs) {
+        const auto& aiter = attrs.find(RGW_ATTR_ACL);
+        bufferlist& bl = aiter->second;
+        RGWAccessControlPolicy policy;
+        ACLOwner owner;
+        try {
+          decode(policy, bl);
+          owner = policy.get_owner();
+        } catch (buffer::error& err) {
+          cerr << " Decode policy failed: " << std::endl;
+          return -EIO;
+        }
+
+        owner.set_id(bucket_info.owner);
+        owner.set_name(user_info.display_name);
+        policy.set_owner(owner);
+
+        bl.clear();
+        encode(policy, bl);
+
+        const rgw_obj r_obj(bucket_info.bucket, obj.key);
+        ret = modify_obj_attr(store, obj_ctx, bucket_info, r_obj, RGW_ATTR_ACL, bl);
+        if (ret < 0) {
+          cerr << " Modify attr failed: "<< ret << std::endl;
+          return ret;
+        }
+      }// for loop
+    } while(is_truncated);
+  } // if
 
   if (opt_cmd == OPT_LOG_LIST) {
     // filter by date?