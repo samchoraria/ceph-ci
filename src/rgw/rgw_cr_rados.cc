ceph/ceph

rgw: add perf counters to fetch_remote_obj()
Signed-off-by: Casey Bodley <cbodley@redhat.com>

src/rgw/rgw_cr_rados.cc
@@ -6,6 +6,7 @@
 #include "rgw_zone.h"
 #include "rgw_coroutine.h"
 #include "rgw_cr_rados.h"
+#include "rgw_sync_counters.h"
 
 #include "services/svc_zone.h"
 #include "services/svc_zone_utils.h"
@@ -585,6 +586,7 @@ int RGWAsyncFetchRemoteObj::_send_request()
 
   rgw_obj dest_obj(bucket_info.bucket, dest_key.value_or(key));
 
+  std::optional<uint64_t> bytes_transferred;
   int r = store->fetch_remote_obj(obj_ctx,
                        user_id,
                        NULL, /* req_info */
@@ -611,10 +613,20 @@ int RGWAsyncFetchRemoteObj::_send_request()
                        NULL, /* string *petag, */
                        NULL, /* void (*progress_cb)(off_t, void *), */
                        NULL, /* void *progress_data*); */
-                       &zones_trace);
+                       &zones_trace,
+                       &bytes_transferred);
 
   if (r < 0) {
     ldout(store->ctx(), 0) << "store->fetch_remote_obj() returned r=" << r << dendl;
+    if (counters) {
+      counters->inc(sync_counters::l_fetch_err, 1);
+    }
+  } else if (counters) {
+    if (bytes_transferred) {
+      counters->inc(sync_counters::l_fetch, *bytes_transferred);
+    } else {
+      counters->inc(sync_counters::l_fetch_not_modified);
+    }
   }
   return r;
 }

src/rgw/rgw_rados.cc
@@ -4280,7 +4280,8 @@ int RGWRados::fetch_remote_obj(RGWObjectCtx& obj_ctx,
                string *petag,
                void (*progress_cb)(off_t, void *),
                void *progress_data,
-               rgw_zone_set *zones_trace)
+               rgw_zone_set *zones_trace,
+               std::optional<uint64_t>* bytes_transferred)
 {
   /* source is in a different zonegroup, copy from there */
 
@@ -4494,6 +4495,9 @@ int RGWRados::fetch_remote_obj(RGWObjectCtx& obj_ctx,
     goto set_err_state;
   }
 
+  if (bytes_transferred) {
+    *bytes_transferred = cb.get_data_len();
+  }
   return 0;
 set_err_state:
   if (copy_if_newer && ret == -ERR_NOT_MODIFIED) {

src/rgw/rgw_rados.h
@@ -1919,7 +1919,8 @@ public:
                        string *petag,
                        void (*progress_cb)(off_t, void *),
                        void *progress_data,
-                       rgw_zone_set *zones_trace= nullptr);
+                       rgw_zone_set *zones_trace= nullptr,
+                       std::optional<uint64_t>* bytes_transferred = 0);
   /**
    * Copy an object.
    * dest_obj: the object to copy into

samchoraria/ceph-ci

(cherry picked from commit 5fcb59060bb03c532e0cf03b32352b046c3f9543)
Conflicts:
	src/rgw/rgw_cr_rados.cc - std::optional -> boost
	src/rgw/rgw_cr_rados.h
	src/rgw/rgw_data_sync.cc
	src/rgw/rgw_rados.cc
Resolves: rhbz#1684642
(cherry picked from commit 70ba7017b957717146c319be7443ae470fee8622)
	src/rgw/rgw_cr_rados.cc zones_trace by value

src/rgw/rgw_cr_rados.cc
@@ -2,6 +2,7 @@
 #include "rgw_rados.h"
 #include "rgw_coroutine.h"
 #include "rgw_cr_rados.h"
+#include "rgw_sync_counters.h"
 
 #include "cls/lock/cls_lock_client.h"
 #include "cls/rgw/cls_rgw_client.h"
@@ -558,6 +559,7 @@ int RGWAsyncFetchRemoteObj::_send_request()
 
   rgw_obj dest_obj(src_obj);
 
+  boost::optional<uint64_t> bytes_transferred;
   int r = store->fetch_remote_obj(obj_ctx,
                        user_id,
                        client_id,
@@ -587,10 +589,20 @@ int RGWAsyncFetchRemoteObj::_send_request()
                        NULL, /* string *petag, */
                        NULL, /* void (*progress_cb)(off_t, void *), */
                        NULL, /* void *progress_data*); */
-                       &zones_trace);
+                       &zones_trace,
+                       &bytes_transferred);
 
   if (r < 0) {
     ldout(store->ctx(), 0) << "store->fetch_remote_obj() returned r=" << r << dendl;
+    if (counters) {
+      counters->inc(sync_counters::l_fetch_err, 1);
+    }
+  } else if (counters) {
+    if (bytes_transferred) {
+      counters->inc(sync_counters::l_fetch, *bytes_transferred);
+    } else {
+      counters->inc(sync_counters::l_fetch_not_modified);
+    }
   }
   return r;
 }

src/rgw/rgw_rados.cc
@@ -8054,7 +8054,8 @@ int RGWRados::fetch_remote_obj(RGWObjectCtx& obj_ctx,
                ceph::buffer::list *petag,
                void (*progress_cb)(off_t, void *),
                void *progress_data,
-               rgw_zone_set *zones_trace)
+               rgw_zone_set *zones_trace,
+               boost::optional<uint64_t>* bytes_transferred)
 {
   /* source is in a different zonegroup, copy from there */
 
@@ -8270,6 +8271,9 @@ int RGWRados::fetch_remote_obj(RGWObjectCtx& obj_ctx,
     delete opstate;
   }
 
+  if (bytes_transferred) {
+    *bytes_transferred = cb.get_data_len();
+  }
   return 0;
 set_err_state:
   if (copy_if_newer && ret == -ERR_NOT_MODIFIED) {

src/rgw/rgw_rados.h
@@ -3178,7 +3178,8 @@ public:
                        ceph::buffer::list *petag,
                        void (*progress_cb)(off_t, void *),
                        void *progress_data,
-                       rgw_zone_set *zones_trace= nullptr);
+                       rgw_zone_set *zones_trace= nullptr,
+                       boost::optional<uint64_t>* bytes_transferred = 0);
   /**
    * Copy an object.
    * dest_obj: the object to copy into