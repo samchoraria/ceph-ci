athanatos/ceph

restful: Expose perf counters
We currently do not expose perf counters in the restful module but this
is a useful piece of information to be exposed by the restful module.

Since we already have a function to gather all the information about the
perf counters, we can make this easily accessible through the restful
API.

This also supports filtering by daemons through regexps.

Signed-off-by: Boris Ranto <branto@redhat.com>

src/pybind/mgr/restful/api/perf.py
@@ -1,0 +1,27 @@
+from pecan import expose, request, response
+from pecan.rest import RestController
+
+from restful import context
+from restful.decorators import auth, lock, paginate
+
+import re
+
+class Perf(RestController):
+    @expose(template='json')
+    @paginate
+    @auth
+    def get(self, **kwargs):
+        """
+        List all the available performance counters
+
+        Options:
+         - 'daemon' -- filter by daemon, accepts Python regexp
+        """
+
+        counters = context.instance.get_all_perf_counters()
+
+        if 'daemon' in kwargs:
+            _re = re.compile(kwargs['daemon'])
+            counters = {k: v for k, v in counters.items() if _re.match(k)}
+
+        return counters

samchoraria/ceph-ci

(cherry picked from commit aaaef9146cd2a2c6511ea8a0f70612b84f476bd7)
Resolves: rhbz#1669838
Conflicts:
	src/pybind/mgr/restful/api/__init__.py: Trivial resolution
	src/pybind/mgr/restful/api/perf.py: s/context/module/g

src/pybind/mgr/restful/api/perf.py
@@ -1,0 +1,27 @@
+from pecan import expose, request, response
+from pecan.rest import RestController
+
+from restful import module
+from restful.decorators import auth, lock, paginate
+
+import re
+
+class Perf(RestController):
+    @expose(template='json')
+    @paginate
+    @auth
+    def get(self, **kwargs):
+        """
+        List all the available performance counters
+
+        Options:
+         - 'daemon' -- filter by daemon, accepts Python regexp
+        """
+
+        counters = module.instance.get_all_perf_counters()
+
+        if 'daemon' in kwargs:
+            _re = re.compile(kwargs['daemon'])
+            counters = {k: v for k, v in counters.items() if _re.match(k)}
+
+        return counters