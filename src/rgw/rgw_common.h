ceph/ceph

rgw_common: introduce a decode_bl function to decode a bl to type
Signed-off-by: Abhishek Lekshmanan <abhishek@suse.com>

src/rgw/rgw_common.h
@@ -2669,4 +2669,16 @@ static inline string rgw_bl_str(ceph::buffer::list& raw)
   return s;
 }
 
+template <typename T>
+int decode_bl(bufferlist& bl, T& t)
+{
+  auto iter = bl.cbegin();
+  try {
+    decode(t, iter);
+  } catch (buffer::error& err) {
+    return -EIO;
+  }
+  return 0;
+}
+
 #endif

samchoraria/ceph-ci

Resolves: rhbz#1703557
(cherry picked from commit 5e56ea6f0778973cdbb90eaf643fba1240af3f8b)
Signed-off-by: Matt Benjamin <mbenjamin@redhat.com>

src/rgw/rgw_common.h
@@ -2489,4 +2489,16 @@ static inline ssize_t rgw_unescape_str(const string& s, ssize_t ofs,
   return string::npos;
 }
 
+template <typename T>
+int decode_bl(bufferlist& bl, T& t)
+{
+  auto iter = bl.begin();
+  try {
+    decode(t, iter);
+  } catch (buffer::error& err) {
+    return -EIO;
+  }
+  return 0;
+}
+
 #endif