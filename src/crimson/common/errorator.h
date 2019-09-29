3a01360369bb979bb6e1b479fdc54fce20b835ac
Radoslaw Zarzynski <rzarzyns@redhat.com>
2019-09-29 15:02:06 +0200

    crimson: drop as_plain_future() from errorated futures.
    Signed-off-by: Radoslaw Zarzynski <rzarzyns@redhat.com>

--- src/crimson/common/errorator.h
+++ src/crimson/common/errorator.h
@@ -352,7 +352,7 @@ struct errorator {
         typename ErroratedFuture<ValuesT...>::errorator_type;
       static_assert(DestErrorator::template contains_once_v<errorator_type>,
                     "conversion is possible to more-or-eq errorated future!");
-      return std::move(*this).as_plain_future();
+      return static_cast<base_t&&>(*this);
     }
 
     // initialize future as failed without throwing. `make_exception_future()`
@@ -475,9 +475,6 @@ struct errorator {
     template <class...>
     friend class errorator;
 
-    base_t&& as_plain_future() && {
-      return std::move(*this);
-    }
   };
 
   // the visitor that forwards handling of all errors to next continuation
@@ -574,7 +571,7 @@ private:
   }
   template <class Arg>
   static decltype(auto) plainify(Arg&& arg) {
-    return std::forward<Arg>(arg).as_plain_future();
+    return std::forward<typename Arg::base_t>(arg);
   }
 
   template <class T, class = std::void_t<T>>
