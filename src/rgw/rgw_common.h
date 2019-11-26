38f7f03fb78a15baddb3e1894df415fa5a5a482c
yuliyang <yuliyang@cmss.chinamobile.com>
2019-11-26 17:59:11 +0800

    rgw: use STSEngine::authenticate when post upload with x_amz_security_token
    Signed-off-by: yuliyang <yuliyang@cmss.chinamobile.com>

--- src/rgw/rgw_common.h
+++ src/rgw/rgw_common.h
@@ -1644,6 +1644,7 @@ namespace rgw {
   namespace auth {
     namespace s3 {
       class AWSBrowserUploadAbstractor;
+      class STSEngine;
     }
     class Completer;
   }
@@ -2053,6 +2054,7 @@ struct req_state : DoutPrefixProvider {
       friend class RGWPostObj_ObjStore_S3;
       /* Reader. */
       friend class rgw::auth::s3::AWSBrowserUploadAbstractor;
+      friend class rgw::auth::s3::STSEngine;
 
       std::string access_key;
       std::string signature;
