ee0410ca3a160756b8bed42097a0c5146d3280f5
Thomas Bechtold <tbechtold@suse.com>
2019-12-09 17:27:46 +0100

    qa: Run flake8 on python2 and python3
    To be able to catch problems with python2 *and* python3, run flake8
    with both versions. From the flake8 homepage:
    
    It is very important to install Flake8 on the correct version of
    Python for your needs. If you want Flake8 to properly parse new
    language features in Python 3.5 (for example), you need it to be
    installed on 3.5 for Flake8 to understand those features. In many
    ways, Flake8 is tied to the version of Python on which it runs.
    
    Also fix the problems with python3 on the way.
    Note: This requires now the six module for teuthology. But this is
    already an install_require in teuthology itself.
    
    Signed-off-by: Thomas Bechtold <tbechtold@suse.com>
    (cherry picked from commit bdcc94a1d1f659b1524fdc6bb8bd1da6d38a30d2)
    
    Conflicts:
    	qa/CMakeLists.txt
    	qa/tasks/ceph_manager.py
    	qa/tasks/cephfs/xfstests_dev.py
    	qa/tasks/cram.py
    	qa/tasks/manypools.py
    	qa/tasks/ragweed.py
    	qa/tasks/s3tests.py
    	qa/tasks/vstart_runner.py
    	qa/tasks/workunit.py
    	qa/tox.ini: trivial resolutions

--- qa/tasks/ceph_manager.py
+++ qa/tasks/ceph_manager.py
@@ -23,6 +23,7 @@ from teuthology.contextutil import safe_while
 from teuthology.orchestra.remote import Remote
 from teuthology.orchestra import run
 from teuthology.exceptions import CommandFailedError
+import six
 
 try:
     from subprocess import DEVNULL # py3k
@@ -1630,7 +1631,7 @@ class CephManager:
         :param erasure_code_use_overwrites: if true, allow overwrites
         """
         with self.lock:
-            assert isinstance(pool_name, basestring)
+            assert isinstance(pool_name, six.string_types)
             assert isinstance(pg_num, int)
             assert pool_name not in self.pools
             self.log("creating pool_name %s" % (pool_name,))
@@ -1682,7 +1683,7 @@ class CephManager:
         :param pool_name: Pool to be removed
         """
         with self.lock:
-            assert isinstance(pool_name, basestring)
+            assert isinstance(pool_name, six.string_types)
             assert pool_name in self.pools
             self.log("removing pool_name %s" % (pool_name,))
             del self.pools[pool_name]
@@ -1701,7 +1702,7 @@ class CephManager:
         Return the number of pgs in the pool specified.
         """
         with self.lock:
-            assert isinstance(pool_name, basestring)
+            assert isinstance(pool_name, six.string_types)
             if pool_name in self.pools:
                 return self.pools[pool_name]
             return 0
@@ -1713,8 +1714,8 @@ class CephManager:
         :returns: property as an int value.
         """
         with self.lock:
-            assert isinstance(pool_name, basestring)
-            assert isinstance(prop, basestring)
+            assert isinstance(pool_name, six.string_types)
+            assert isinstance(prop, six.string_types)
             output = self.raw_cluster_cmd(
                 'osd',
                 'pool',
@@ -1732,8 +1733,8 @@ class CephManager:
         This routine retries if set operation fails.
         """
         with self.lock:
-            assert isinstance(pool_name, basestring)
-            assert isinstance(prop, basestring)
+            assert isinstance(pool_name, six.string_types)
+            assert isinstance(prop, six.string_types)
             assert isinstance(val, int)
             tries = 0
             while True:
@@ -1760,7 +1761,7 @@ class CephManager:
         Increase the number of pgs in a pool
         """
         with self.lock:
-            assert isinstance(pool_name, basestring)
+            assert isinstance(pool_name, six.string_types)
             assert isinstance(by, int)
             assert pool_name in self.pools
             if self.get_num_creating() > 0:
@@ -1780,7 +1781,7 @@ class CephManager:
         with self.lock:
             self.log('contract_pool %s by %s min %s' % (
                      pool_name, str(by), str(min_pgs)))
-            assert isinstance(pool_name, basestring)
+            assert isinstance(pool_name, six.string_types)
             assert isinstance(by, int)
             assert pool_name in self.pools
             if self.get_num_creating() > 0:
@@ -1816,7 +1817,7 @@ class CephManager:
         Set pgpnum property of pool_name pool.
         """
         with self.lock:
-            assert isinstance(pool_name, basestring)
+            assert isinstance(pool_name, six.string_types)
             assert pool_name in self.pools
             if not force and self.get_num_creating() > 0:
                 return False
