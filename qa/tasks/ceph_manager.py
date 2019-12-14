522b18b342bc58b7a2a5fc9ed463d0857bf72f5d
Kyr Shatskyy <kyrylo.shatskyy@suse.com>
2019-12-14 01:16:34 +0100

    qa/tasks: fix imports for py3 compatibility
    Signed-off-by: Kyr Shatskyy <kyrylo.shatskyy@suse.com>

--- qa/tasks/ceph_manager.py
+++ qa/tasks/ceph_manager.py
@@ -18,8 +18,8 @@ from io import BytesIO
 from teuthology.util.compat import stringify
 from teuthology import misc as teuthology
 from tasks.scrub import Scrubber
-from util.rados import cmd_erasure_code_profile
-from util import get_remote
+from tasks.util.rados import cmd_erasure_code_profile
+from tasks.util import get_remote
 from teuthology.contextutil import safe_while
 from teuthology.orchestra.remote import Remote
 from teuthology.orchestra import run
