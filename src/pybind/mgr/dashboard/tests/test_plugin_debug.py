athanatos/ceph

mgr/dashboard: fix LazyUUID4 not serializable
- Patch older versions of cherrypy (<11.1.0) to include `unique_id`
field in Request items.
- Add unit tests to verify Debug mode plugin
- Fix RGW Client test
- Fix Tools test

Fixes: https://tracker.ceph.com/issues/42565
Signed-off-by: Ernesto Puerta <epuertat@redhat.com>

src/pybind/mgr/dashboard/tests/test_plugin_debug.py
@@ -1,0 +1,38 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import
+
+from . import CLICommandTestMixin, ControllerTestCase
+
+
+class TestPluginDebug(ControllerTestCase, CLICommandTestMixin):
+    @classmethod
+    def setup_server(cls):
+        # pylint: disable=protected-access
+        cls.setup_controllers([])
+
+    def setUp(self):
+        self.mock_kv_store()
+
+    def test_debug_disabled(self):
+        self.exec_cmd('debug', action='disable')
+
+        self._get('/api/unexisting_controller')
+        self.assertStatus(404)
+
+        data = self.json_body()
+        self.assertGreater(len(data), 0)
+        self.assertNotIn('traceback', data)
+        self.assertNotIn('version', data)
+        self.assertIn('request_id', data)
+
+    def test_debug_enabled(self):
+        self.exec_cmd('debug', action='enable')
+
+        self._get('/api/unexisting_controller')
+        self.assertStatus(404)
+
+        data = self.json_body()
+        self.assertGreater(len(data), 0)
+        self.assertIn('traceback', data)
+        self.assertIn('version', data)
+        self.assertIn('request_id', data)

samchoraria/ceph-ci

(cherry picked from commit ae6ab3047b92b82b6aa03dc26dd9b10ebb4264aa)
Conflicts:
  - src/pybind/mgr/dashboard/tests/test_plugin_debug.py (json_body to
      jsonBody, as 38c76db32db4e36a0303dc9321404c9eaad30ecf (#29604) has
      not been backported yet)

src/pybind/mgr/dashboard/tests/test_plugin_debug.py
@@ -1,0 +1,38 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import
+
+from . import CLICommandTestMixin, ControllerTestCase
+
+
+class TestPluginDebug(ControllerTestCase, CLICommandTestMixin):
+    @classmethod
+    def setup_server(cls):
+        # pylint: disable=protected-access
+        cls.setup_controllers([])
+
+    def setUp(self):
+        self.mock_kv_store()
+
+    def test_debug_disabled(self):
+        self.exec_cmd('debug', action='disable')
+
+        self._get('/api/unexisting_controller')
+        self.assertStatus(404)
+
+        data = self.jsonBody()
+        self.assertGreater(len(data), 0)
+        self.assertNotIn('traceback', data)
+        self.assertNotIn('version', data)
+        self.assertIn('request_id', data)
+
+    def test_debug_enabled(self):
+        self.exec_cmd('debug', action='enable')
+
+        self._get('/api/unexisting_controller')
+        self.assertStatus(404)
+
+        data = self.jsonBody()
+        self.assertGreater(len(data), 0)
+        self.assertIn('traceback', data)
+        self.assertIn('version', data)
+        self.assertIn('request_id', data)