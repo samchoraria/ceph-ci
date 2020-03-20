59d120dbf474d82c8c795b5cabff09b29dbd1293
Mykola Golub <mgolub@suse.com>
2020-03-20 16:19:25 +0000

    ceph-erasure-code-tool: new tool to encode/decode files
    E.g. it may be useful as a last resort when recovering an object from
    a damaged PG: extract the encoded object chunks from the PG shards
    with ceph-objectstore-tool and then decode with ceph-erasure-code-tool.
    
    Signed-off-by: Mykola Golub <mgolub@suse.com>

---
+++ src/test/ceph-erasure-code-tool/test_ceph-erasure-code-tool.sh
@@ -0,0 +1,31 @@
+#!/bin/sh -ex
+
+TMPDIR=/tmp/test_ceph-erasure-code-tool.$$
+mkdir $TMPDIR
+trap "rm -fr $TMPDIR" 0
+
+dd if="$(which ceph-erasure-code-tool)" of=$TMPDIR/data bs=770808 count=1
+cp $TMPDIR/data $TMPDIR/data.orig
+
+ceph-erasure-code-tool encode \
+                       plugin=jerasure,technique=reed_sol_van,k=2,m=1 \
+                       4096 \
+                       0,1,2 \
+                       $TMPDIR/data
+test -f $TMPDIR/data.0
+test -f $TMPDIR/data.1
+test -f $TMPDIR/data.2
+
+rm $TMPDIR/data
+
+ceph-erasure-code-tool decode \
+                       plugin=jerasure,technique=reed_sol_van,k=2,m=1 \
+                       4096 \
+                       0,2 \
+                       $TMPDIR/data
+
+size=$(stat -c '%s' $TMPDIR/data.orig)
+truncate -s "${size}" $TMPDIR/data # remove stripe width padding
+cmp $TMPDIR/data.orig $TMPDIR/data
+
+echo OK
