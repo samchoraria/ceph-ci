9e88e74baef66f27ebdf0d168296249d6bded68a
Sebastian Wagner <sebastian.wagner@suse.com>
2020-04-24 15:52:07 +0200

    pybind/mgr: Remove insights/tox.ini
    All we need is part of mgr/tox.ini nowadays.
    
    Signed-off-by: Sebastian Wagner <sebastian.wagner@suse.com>

--- src/pybind/mgr/insights/__init__.py
+++ src/pybind/mgr/insights/__init__.py
@@ -1,9 +1,6 @@
-from __future__ import absolute_import
 import os
 
 if 'UNITTEST' not in os.environ:
-    from .module import Module
-else:
-    import sys
-    import mock
-    sys.modules['ceph_module'] = mock.Mock()
+    import tests
+
+from .module import Module
