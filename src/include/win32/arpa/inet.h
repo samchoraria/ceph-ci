petrutlucian94/ceph

[win32] Add compatibility layer
Windows provides socket and other related structures that are mostly
following the Posix standards, but using different headers.

Rather than adding lots of platform checks and require future commits
to do so as well, we're adding headers with the same names, which
in turn include the required Windows headers.

In a few cases, some functions declared by unistd.h are actually
defined by different Windows headers, so we'll need additional includes.

One thing to note here is that boost requires us to include aio.hpp
before the Windows socket headers.

Signed-off-by: Lucian Petrut <lpetrut@cloudbasesolutions.com>

src/include/windows/arpa/inet.h
@@ -1,0 +1,1 @@
+#include "windefs.h"

src/include/windows/sys/un.h
@@ -1,0 +1,1 @@
+#include "include/windows/windefs.h"

samchoraria/ceph-ci

src/include/win32/arpa/inet.h
@@ -1,0 +1,1 @@
+#include "winsock_compat.h"

src/include/win32/sys/un.h
@@ -1,0 +1,1 @@
+#include "include/win32/winsock_compat.h"