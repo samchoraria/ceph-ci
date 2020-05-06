694cf8c91d27027e06db03bbe69a96bba68de029
Alfonso Martínez <almartin@redhat.com>
2020-05-06 14:26:22 +0200

    mgr/dashboard: grafana dashboards for rgw multisite sync performance
    * RGW sync perf. counters are now exposed through grafana dashboards.
    * Sync Performance tab is only shown if rgw realm is detected.
    * Prometheus module: added metrics suitable for prometheus consumption (from existing ones, not replacing for backward compatibility).
    
    Fixes: https://tracker.ceph.com/issues/45310
    Signed-off-by: Alfonso Martínez <almartin@redhat.com>

--- src/pybind/mgr/dashboard/frontend/src/app/ceph/rgw/rgw-daemon-list/rgw-daemon-list.component.spec.ts
+++ src/pybind/mgr/dashboard/frontend/src/app/ceph/rgw/rgw-daemon-list/rgw-daemon-list.component.spec.ts
@@ -4,8 +4,12 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RouterTestingModule } from '@angular/router/testing';
 
 import { TabsModule } from 'ngx-bootstrap/tabs';
+import { of } from 'rxjs';
 
 import { configureTestBed, i18nProviders } from '../../../../testing/unit-test-helper';
+import { RgwSiteService } from '../../../shared/api/rgw-site.service';
+import { Permissions } from '../../../shared/models/permissions';
+import { AuthStorageService } from '../../../shared/services/auth-storage.service';
 import { SharedModule } from '../../../shared/shared.module';
 import { PerformanceCounterModule } from '../../performance-counter/performance-counter.module';
 import { RgwDaemonDetailsComponent } from '../rgw-daemon-details/rgw-daemon-details.component';
@@ -14,6 +18,9 @@ import { RgwDaemonListComponent } from './rgw-daemon-list.component';
 describe('RgwDaemonListComponent', () => {
   let component: RgwDaemonListComponent;
   let fixture: ComponentFixture<RgwDaemonListComponent>;
+  let getPermissionsSpy: jasmine.Spy;
+  let getRealmsSpy: jasmine.Spy;
+  const permissions = new Permissions({ grafana: ['read'] });
 
   configureTestBed({
     declarations: [RgwDaemonListComponent, RgwDaemonDetailsComponent],
@@ -29,12 +36,42 @@ describe('RgwDaemonListComponent', () => {
   });
 
   beforeEach(() => {
+    getPermissionsSpy = spyOn(TestBed.get(AuthStorageService), 'getPermissions');
+    getPermissionsSpy.and.returnValue(new Permissions({}));
+    getRealmsSpy = spyOn(TestBed.get(RgwSiteService), 'get');
+    getRealmsSpy.and.returnValue(of([]));
     fixture = TestBed.createComponent(RgwDaemonListComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should only show Daemons List tab', () => {
+    fixture.detectChanges();
+
+    const tabs = fixture.debugElement.nativeElement.querySelectorAll('tab');
+    expect(tabs.length).toEqual(1);
+    expect(tabs[0].getAttribute('heading')).toEqual('Daemons List');
+  });
+
+  it('should show Overall Performance tab', () => {
+    getPermissionsSpy.and.returnValue(permissions);
+    fixture.detectChanges();
+
+    const tabs = fixture.debugElement.nativeElement.querySelectorAll('tab');
+    expect(tabs.length).toEqual(2);
+    expect(tabs[1].getAttribute('heading')).toEqual('Overall Performance');
+  });
+
+  it('should show Sync Performance tab', () => {
+    getPermissionsSpy.and.returnValue(permissions);
+    getRealmsSpy.and.returnValue(of(['realm1']));
+    fixture.detectChanges();
+
+    const tabs = fixture.debugElement.nativeElement.querySelectorAll('tab');
+    expect(tabs.length).toEqual(3);
+    expect(tabs[2].getAttribute('heading')).toEqual('Sync Performance');
   });
 });
