c27b18b0e6160aa3c9e113157e2dbd6628aa9a43
Mykola Golub <mgolub@suse.com>
2020-05-15 11:29:20 +0100

    qa/workunits/rbd: tweaks to troubleshoot TestLibRBD.QuiesceWatchTimeout
    Signed-off-by: Mykola Golub <mgolub@suse.com>

--- qa/workunits/rbd/test_librbd.sh
+++ qa/workunits/rbd/test_librbd.sh
@@ -4,6 +4,6 @@ if [ -n "${VALGRIND}" ]; then
   valgrind ${VALGRIND} --suppressions=${TESTDIR}/valgrind.supp \
     --error-exitcode=1 ceph_test_librbd
 else
-  ceph_test_librbd
+  CEPH_ARGS='--debug-rbd=30' ceph_test_librbd --gtest_filter=TestLibRBD.QuiesceWatchTimeout --gtest_repeat=100
 fi
 exit 0
