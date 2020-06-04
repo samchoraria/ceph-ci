athanatos/ceph

crimson/os/seastore: add lba_manager interface with btree_lba_manager impl
Signed-off-by: Samuel Just <sjust@redhat.com>

src/crimson/os/seastore/lba_manager.h
@@ -1,0 +1,149 @@
+// -*- mode:C++; tab-width:8; c-basic-offset:2; indent-tabs-mode:t -*-
+// vim: ts=8 sw=2 smarttab
+
+#pragma once
+
+#include <iostream>
+
+#include <boost/intrusive_ptr.hpp>
+#include <boost/smart_ptr/intrusive_ref_counter.hpp>
+
+#include <seastar/core/future.hh>
+
+#include "include/ceph_assert.h"
+#include "include/buffer_fwd.h"
+#include "include/interval_set.h"
+#include "common/interval_map.h"
+
+#include "crimson/osd/exceptions.h"
+
+#include "crimson/os/seastore/cache.h"
+#include "crimson/os/seastore/seastore_types.h"
+#include "crimson/os/seastore/segment_manager.h"
+
+namespace crimson::os::seastore {
+
+class LBAPin {
+public:
+  virtual extent_len_t get_length() const = 0;
+  virtual paddr_t get_paddr() const = 0;
+  virtual laddr_t get_laddr() const = 0;
+  virtual unsigned get_refcount() const = 0;
+
+  virtual ~LBAPin() {}
+};
+using LBAPinRef = std::unique_ptr<LBAPin>;
+std::ostream &operator<<(std::ostream &out, const LBAPin &rhs);
+
+using lba_pin_list_t = std::list<LBAPinRef>;
+
+std::ostream &operator<<(std::ostream &out, const lba_pin_list_t &rhs);
+
+/**
+ * Abstract interface for managing the logical to physical mapping
+ */
+class LBAManager {
+public:
+  using mkfs_ertr = crimson::errorator<
+    crimson::ct_error::input_output_error>;
+  using mkfs_ret = mkfs_ertr::future<>;
+  virtual mkfs_ret mkfs(
+    Transaction &t
+  ) = 0;
+
+  /**
+   * Fetches mappings for laddr_t in range [offset, offset + len)
+   *
+   * Future will not result until all pins have resolved (set_paddr called)
+   */
+  using get_mapping_ertr = crimson::errorator<
+  crimson::ct_error::input_output_error>;
+  using get_mapping_ret = get_mapping_ertr::future<lba_pin_list_t>;
+  virtual get_mapping_ret get_mapping(
+    Transaction &t,
+    laddr_t offset, extent_len_t length) = 0;
+
+  /**
+   * Fetches mappings for laddr_t in range [offset, offset + len)
+   *
+   * Future will not result until all pins have resolved (set_paddr called)
+   */
+  using get_mappings_ertr = crimson::errorator<
+    crimson::ct_error::input_output_error>;
+  using get_mappings_ret = get_mapping_ertr::future<lba_pin_list_t>;
+  virtual get_mappings_ret get_mappings(
+    Transaction &t,
+    laddr_list_t &&extent_lisk) = 0;
+
+  /**
+   * Allocates a new mapping referenced by LBARef
+   *
+   * Offset will be relative to the block offset of the record
+   * This mapping will block from transaction submission until set_paddr
+   * is called on the LBAPin.
+   */
+  using alloc_extent_ertr = crimson::errorator<
+    crimson::ct_error::input_output_error>;
+  using alloc_extent_ret = alloc_extent_ertr::future<LBAPinRef>;
+  virtual alloc_extent_ret alloc_extent(
+    Transaction &t,
+    laddr_t hint,
+    extent_len_t len,
+    paddr_t addr) = 0;
+
+  /**
+   * Creates a new absolute mapping.
+   *
+   * off~len must be unreferenced
+   */
+  using set_extent_ertr = crimson::errorator<
+    crimson::ct_error::input_output_error,
+    crimson::ct_error::invarg>;
+  using set_extent_ret = set_extent_ertr::future<LBAPinRef>;
+  virtual set_extent_ret set_extent(
+    Transaction &t,
+    laddr_t off, extent_len_t len, paddr_t addr) = 0;
+
+  using ref_ertr = crimson::errorator<
+    crimson::ct_error::enoent,
+    crimson::ct_error::input_output_error>;
+
+  /**
+   * Decrements ref count on extent
+   *
+   * @return true if freed
+   */
+  using decref_extent_ret = ref_ertr::future<bool>;
+  virtual decref_extent_ret decref_extent(
+    Transaction &t,
+    laddr_t addr) = 0;
+
+  /**
+   * Increments ref count on extent
+   */
+  using incref_extent_ret = ref_ertr::future<>;
+  virtual incref_extent_ret incref_extent(
+    Transaction &t,
+    laddr_t addr) = 0;
+
+  // TODO: probably unused, removed
+  using submit_lba_transaction_ertr = crimson::errorator<
+    crimson::ct_error::input_output_error>;
+  using submit_lba_transaction_ret = submit_lba_transaction_ertr::future<>;
+  virtual submit_lba_transaction_ret submit_lba_transaction(
+    Transaction &t) = 0;
+
+  virtual TransactionRef create_transaction() = 0;
+
+  virtual ~LBAManager() {}
+};
+using LBAManagerRef = std::unique_ptr<LBAManager>;
+
+class Cache;
+namespace lba_manager {
+LBAManagerRef create_lba_manager(
+  SegmentManager &segment_manager,
+  Cache &cache);
+}
+
+}

src/crimson/os/seastore/seastore_types.h
@@ -6,6 +6,7 @@
 #include <limits>
 #include <iostream>
 
+#include "include/byteorder.h"
 #include "include/denc.h"
 #include "include/buffer.h"
 #include "include/cmp.h"
@@ -148,6 +149,22 @@ constexpr paddr_t make_block_relative_paddr(segment_off_t off) {
   return paddr_t{BLOCK_REL_SEG_ID, off};
 }
 
+struct paddr_le_t {
+  ceph_le32 segment = ceph_le32(NULL_SEG_ID);
+  ceph_les32 offset = ceph_les32(NULL_SEG_OFF);
+
+  paddr_le_t() = default;
+  paddr_le_t(ceph_le32 segment, ceph_les32 offset)
+    : segment(segment), offset(offset) {}
+  paddr_le_t(segment_id_t segment, segment_off_t offset)
+    : segment(ceph_le32(segment)), offset(ceph_les32(offset)) {}
+  paddr_le_t(const paddr_t &addr) : paddr_le_t(addr.segment, addr.offset) {}
+
+  operator paddr_t() const {
+    return paddr_t{segment, offset};
+  }
+};
+
 std::ostream &operator<<(std::ostream &out, const paddr_t &rhs);
 
 // logical addr, see LBAManager, TransactionManager
@@ -158,10 +175,14 @@ constexpr laddr_t L_ADDR_NULL = std::numeric_limits<laddr_t>::max();
 constexpr laddr_t L_ADDR_ROOT = std::numeric_limits<laddr_t>::max() - 1;
 constexpr laddr_t L_ADDR_LBAT = std::numeric_limits<laddr_t>::max() - 2;
 
+using laddr_le_t = ceph_le64;
+
 // logical offset, see LBAManager, TransactionManager
 using extent_len_t = uint32_t;
 constexpr extent_len_t EXTENT_LEN_MAX =
   std::numeric_limits<extent_len_t>::max();
+
+using extent_len_le_t = ceph_le32;
 
 struct laddr_list_t : std::list<std::pair<laddr_t, extent_len_t>> {
   template <typename... T>

samchoraria/ceph-ci

src/crimson/os/seastore/lba_manager.h
@@ -1,0 +1,151 @@
+// -*- mode:C++; tab-width:8; c-basic-offset:2; indent-tabs-mode:t -*-
+// vim: ts=8 sw=2 smarttab
+
+#pragma once
+
+#include <iostream>
+
+#include <boost/intrusive_ptr.hpp>
+#include <boost/smart_ptr/intrusive_ref_counter.hpp>
+
+#include <seastar/core/future.hh>
+
+#include "include/ceph_assert.h"
+#include "include/buffer_fwd.h"
+#include "include/interval_set.h"
+#include "common/interval_map.h"
+
+#include "crimson/osd/exceptions.h"
+
+#include "crimson/os/seastore/cache.h"
+#include "crimson/os/seastore/seastore_types.h"
+#include "crimson/os/seastore/segment_manager.h"
+
+namespace crimson::os::seastore {
+
+class LBAPin;
+using LBAPinRef = std::unique_ptr<LBAPin>;
+class LBAPin {
+public:
+  virtual extent_len_t get_length() const = 0;
+  virtual paddr_t get_paddr() const = 0;
+  virtual laddr_t get_laddr() const = 0;
+  virtual LBAPinRef duplicate() const = 0;
+
+  virtual ~LBAPin() {}
+};
+std::ostream &operator<<(std::ostream &out, const LBAPin &rhs);
+
+using lba_pin_list_t = std::list<LBAPinRef>;
+
+std::ostream &operator<<(std::ostream &out, const lba_pin_list_t &rhs);
+
+/**
+ * Abstract interface for managing the logical to physical mapping
+ */
+class LBAManager {
+public:
+  using mkfs_ertr = crimson::errorator<
+    crimson::ct_error::input_output_error>;
+  using mkfs_ret = mkfs_ertr::future<>;
+  virtual mkfs_ret mkfs(
+    Transaction &t
+  ) = 0;
+
+  /**
+   * Fetches mappings for laddr_t in range [offset, offset + len)
+   *
+   * Future will not resolve until all pins have resolved (set_paddr called)
+   */
+  using get_mapping_ertr = crimson::errorator<
+  crimson::ct_error::input_output_error>;
+  using get_mapping_ret = get_mapping_ertr::future<lba_pin_list_t>;
+  virtual get_mapping_ret get_mapping(
+    Transaction &t,
+    laddr_t offset, extent_len_t length) = 0;
+
+  /**
+   * Fetches mappings for laddr_t in range [offset, offset + len)
+   *
+   * Future will not result until all pins have resolved (set_paddr called)
+   */
+  using get_mappings_ertr = crimson::errorator<
+    crimson::ct_error::input_output_error>;
+  using get_mappings_ret = get_mapping_ertr::future<lba_pin_list_t>;
+  virtual get_mappings_ret get_mappings(
+    Transaction &t,
+    laddr_list_t &&extent_lisk) = 0;
+
+  /**
+   * Allocates a new mapping referenced by LBARef
+   *
+   * Offset will be relative to the block offset of the record
+   * This mapping will block from transaction submission until set_paddr
+   * is called on the LBAPin.
+   */
+  using alloc_extent_ertr = crimson::errorator<
+    crimson::ct_error::input_output_error>;
+  using alloc_extent_ret = alloc_extent_ertr::future<LBAPinRef>;
+  virtual alloc_extent_ret alloc_extent(
+    Transaction &t,
+    laddr_t hint,
+    extent_len_t len,
+    paddr_t addr) = 0;
+
+  /**
+   * Creates a new absolute mapping.
+   *
+   * off~len must be unreferenced
+   */
+  using set_extent_ertr = crimson::errorator<
+    crimson::ct_error::input_output_error,
+    crimson::ct_error::invarg>;
+  using set_extent_ret = set_extent_ertr::future<LBAPinRef>;
+  virtual set_extent_ret set_extent(
+    Transaction &t,
+    laddr_t off, extent_len_t len, paddr_t addr) = 0;
+
+  using ref_ertr = crimson::errorator<
+    crimson::ct_error::enoent,
+    crimson::ct_error::input_output_error>;
+  using ref_ret = ref_ertr::future<unsigned>;
+
+  /**
+   * Decrements ref count on extent
+   *
+   * @return returns resulting refcount
+   */
+  virtual ref_ret decref_extent(
+    Transaction &t,
+    laddr_t addr) = 0;
+
+  /**
+   * Increments ref count on extent
+   *
+   * @return returns resulting refcount
+   */
+  virtual ref_ret incref_extent(
+    Transaction &t,
+    laddr_t addr) = 0;
+
+  // TODO: probably unused, removed
+  using submit_lba_transaction_ertr = crimson::errorator<
+    crimson::ct_error::input_output_error>;
+  using submit_lba_transaction_ret = submit_lba_transaction_ertr::future<>;
+  virtual submit_lba_transaction_ret submit_lba_transaction(
+    Transaction &t) = 0;
+
+  virtual TransactionRef create_transaction() = 0;
+
+  virtual ~LBAManager() {}
+};
+using LBAManagerRef = std::unique_ptr<LBAManager>;
+
+class Cache;
+namespace lba_manager {
+LBAManagerRef create_lba_manager(
+  SegmentManager &segment_manager,
+  Cache &cache);
+}
+
+}

src/crimson/os/seastore/seastore_types.h
@@ -6,6 +6,7 @@
 #include <limits>
 #include <iostream>
 
+#include "include/byteorder.h"
 #include "include/denc.h"
 #include "include/buffer.h"
 #include "include/cmp.h"
@@ -148,6 +149,22 @@ constexpr paddr_t make_block_relative_paddr(segment_off_t off) {
   return paddr_t{BLOCK_REL_SEG_ID, off};
 }
 
+struct paddr_le_t {
+  ceph_le32 segment = init_le32(NULL_SEG_ID);
+  ceph_les32 offset = init_les32(NULL_SEG_OFF);
+
+  paddr_le_t() = default;
+  paddr_le_t(ceph_le32 segment, ceph_les32 offset)
+    : segment(segment), offset(offset) {}
+  paddr_le_t(segment_id_t segment, segment_off_t offset)
+    : segment(init_le32(segment)), offset(init_les32(offset)) {}
+  paddr_le_t(const paddr_t &addr) : paddr_le_t(addr.segment, addr.offset) {}
+
+  operator paddr_t() const {
+    return paddr_t{segment, offset};
+  }
+};
+
 std::ostream &operator<<(std::ostream &out, const paddr_t &rhs);
 
 // logical addr, see LBAManager, TransactionManager
@@ -158,10 +175,17 @@ constexpr laddr_t L_ADDR_NULL = std::numeric_limits<laddr_t>::max();
 constexpr laddr_t L_ADDR_ROOT = std::numeric_limits<laddr_t>::max() - 1;
 constexpr laddr_t L_ADDR_LBAT = std::numeric_limits<laddr_t>::max() - 2;
 
+using laddr_le_t = ceph_le64;
+
 // logical offset, see LBAManager, TransactionManager
 using extent_len_t = uint32_t;
 constexpr extent_len_t EXTENT_LEN_MAX =
   std::numeric_limits<extent_len_t>::max();
+
+using extent_len_le_t = ceph_le32;
+inline extent_len_le_t init_extent_len_le_t(extent_len_t len) {
+  return init_le32(len);
+}
 
 struct laddr_list_t : std::list<std::pair<laddr_t, extent_len_t>> {
   template <typename... T>